951591c96a39e85518278500379ac0bc3f569126
tsukumar <30828604+tsukumar@users.noreply.github.com>
2018-03-09 16:43:09 +0530

    Create 65.c

---
+++ 65.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+    int i, n,c;
+    char a[20];
+   
+    printf("Enter  ");
+    scanf("%s", &a);
+    if(printf("%s",a))
+    {
+        
+    }
+}
