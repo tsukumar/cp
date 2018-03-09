150466571ddfbabf664824d5942cab149ff5a63e
tsukumar <30828604+tsukumar@users.noreply.github.com>
2018-03-09 16:35:30 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+    int i, n,c;
+   
+    printf("Enter  ");
+    scanf("%d", &n);
+    printf("enter");
+    scanf("%d",&i);
+    c=n+i;
+    if(c%2==0)
+    {
+        printf("even");
+        
+    }
+   else
+   {
+       printf("odd");
+   }
+}
