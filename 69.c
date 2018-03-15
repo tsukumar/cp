2e458465ae2a067a24dc16835aa16f843af53b1d
tsukumar <30828604+tsukumar@users.noreply.github.com>
2018-03-15 16:13:56 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+	int n,m,v;
+	printf("enter the number:");
+	scanf("%d",&m);
+	printf("enter");
+	scanf("%d",&n);
+	v=m+n;
+	
+	if(v%2==0)
+	{
+	printf("yes");
+	
+	}
+	else
+	{
+	   printf("no"); 
+	}
+	
+}
